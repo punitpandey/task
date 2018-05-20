e58e56a9890dfed29e9d9a08a592bd57824faa9a
punit pandey <mailmeonpunit@gmail.com>
2018-05-21 02:32:14 +0530

    final

--- task.py
+++ task.py
@@ -68,7 +68,7 @@ def index():
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USERNAME'] = 'mailmeonpunit@gmail.com'
-app.config['MAIL_PASSWORD'] = 'stephen@123'
+app.config['MAIL_PASSWORD'] = '********'
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 mail = Mail(app)
