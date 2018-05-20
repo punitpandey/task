b696d5d5038561595feca2e885e875ede587697f
punit pandey <mailmeonpunit@gmail.com>
2018-05-21 01:34:18 +0530

    Formatted

--- task.py
+++ task.py
@@ -1,25 +1,44 @@
 from datetime import datetime,timedelta
+from flask import Flask,request,jsonify,render_template
 from time import strftime
 import time
 import os
-from flask import Flask,request,jsonify
 import requests
 import json
+import sqlite3
 from flask_sqlalchemy import SQLAlchemy
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 from flask_mail import Mail, Message
 
-app =Flask(__name__)
-mail=Mail(app)
+basedir=os.path.abspath(os.path.dirname(__file__))
 
+def send_push_notification(target,notification_msg):
+    """to send push notification to specific target"""
+    header = {
+                "Content-Type": "application/json; charset=utf-8",
+                "Authorization": "Basic OWIyYWUyZGEtODhiMS00M2EwLWFiNzItZTJkODI0YmRjYjMz"
+            }
+    payload = {
+                "app_id": "e9af5911-ad44-4157-bd6c-6cfabf2374de",
+                "include_player_ids": target,
+                "contents": {"en": notification_msg}
+            } 
+    req = requests.post(
+                            "https://onesignal.com/api/v1/notifications",
+                            headers=header,
+                            data=json.dumps(payload)
+                        )
+    print(req.status_code, req.reason)
+
+app =Flask(__name__)
 @app.route('/registerNotification',methods=['POST'])
 def registerNotify():
+    """to register user for push notification"""
     if request.method=='POST':
         try:
             user=str(request.form['user'])
             notify_id=str(request.form["notify_id"])
-            basedir=os.path.abspath(os.path.dirname(__file__))
             sql_db = sqlite3.connect(basedir+'/assignment.db')
             sql_cursor = sql_db.cursor()
             sql_cursor.execute(
@@ -28,7 +47,8 @@ def registerNotify():
             teacher_id=str(teacher_data[0][0])#to parse value from tuple returned
             if(teacher_data):
                 try:
-                    sql_cursor.execute("UPDATE teacher set notify_id=? where id=?",(notify_id,teacher_id,))
+                    sql_cursor.execute("UPDATE teacher set notify_id=? where id=?",
+                                        (notify_id,teacher_id,))
                 except Exception as e:
                     return str(e)
             else:
@@ -42,178 +62,106 @@ def registerNotify():
 
 @app.route('/')
 def index():
-    return """<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Dashboard</title>
-    <!--Import Google Icon Font-->
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <!-- Compiled and minified CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css">
-
-    <!-- Compiled and minified JavaScript -->
-    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>
-    <script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" async=""></script>
-    <script>
-    var OneSignal = window.OneSignal || [];
-    
-    function getNotify(){
-        let name=document.getElementById("name").value;
-        // alert(name);
-        // push notify
-        OneSignal.push(function() {
-            OneSignal.on('subscriptionChange', function(isSubscribed) {
-                if (isSubscribed) {
-                // The user is subscribed
-                //   Either the user subscribed for the first time
-                //   Or the user was subscribed -> unsubscribed -> subscribed
-                OneSignal.getUserId( function(userId) {
-                    $.ajax({
-                        url:"/registerNotification",
-                        type:"POST",
-                        data:{
-                            "user":document.getElementById("name").value,
-                            "notify_id":userId
-                        },
-                        success:function(res){
-                            //alert(res);
-                            $('#notifyDiv').hide();
-                            $('#activeDiv').show();
-                        }
-                    });
-                    // Make a POST call to your server with the user ID
-                });
-                }
-            });
-            OneSignal.init({
-            appId: "e9af5911-ad44-4157-bd6c-6cfabf2374de",
-            autoRegister: false,
-            notifyButton: {
-                enable: true,
-                displayPredicate: function() {
-                return OneSignal.isPushNotificationsEnabled()
-                    .then(function(isPushEnabled) {
-                        /* The user is subscribed, so we want to return "false" to hide the Subscription Bell */
-                        $('#notifyDiv').hide();
-                        $('#activeDiv').show();
-                        return !isPushEnabled;
-                    });
-                },
-            },
-            welcomeNotification: {
-                "title": "Task",
-                "message": "Thank You! You will be notified with the updates now.",
-                // "url": "" /* Leave commented for the notification to not open a window on Chrome and Firefox (on Safari, it opens to your webpage) */
-            }
-            });
-        });
-    }
-    </script>
-</head>
-<body>
-    <nav>
-        <div class="nav-wrapper">
-          <a href="#!" class="brand-logo"></a>
-          <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
-          <ul class="right hide-on-med-and-down">
-            <li><a href="/">Home</a></li>
-          </ul>
-        </div>
-      </nav>
-    
-      <ul class="sidenav" id="mobile-demo">
-        <li><a href="/">Home</a></li>
-      </ul>
-      <div class="container">
-          <div id="notifyDiv" class="row">
-            <div class="input-field col s12">
-              <input value="punit" id="name" type="text" class="validate">
-              <label class="active" for="name">Name</label>
-            </div>
-            <button class="btn waves-effect waves-light red lighten-2" type="submit" id="getAlert" name="action" onclick="getNotify()" name="action">Submit
-                <i class="material-icons right">send</i>
-              </button>
-          </div>
-          <div id="activeDiv" style="display: none;">
-            <center><h3>Ready to receive Notifications.</h3></center>
-          </div>
-      </div>
-</body>
-<script>
-    $( document ).ready(function(){
-        $('.sidenav').sidenav();
-    });
-</script>
-</html>"""
+    return render_template('index.html')
 
 # mail configuration
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USERNAME'] = 'mailmeonpunit@gmail.com'
-app.config['MAIL_PASSWORD'] = '**********'
+app.config['MAIL_PASSWORD'] = 'stephen@123'
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 mail = Mail(app)
 
 def task():
-    basedir=os.path.abspath(os.path.dirname(__file__))
+    """task to be checked repeatedly in order to fetch teacher's schedule and notify"""
     sql_db = sqlite3.connect(basedir+'/assignment.db')
     sql_cursor = sql_db.cursor()
     currTime=str(strftime("%H:%M:%S",time.localtime(time.time()+300)))
     # to notify 5 minutes prior to class start
     sql_cursor.execute(
-    "SELECT DISTINCT teacher.id,teacher.name,teacher.email,teacher.notify_id,schedule.subject,schedule.classFrom,schedule.classTo FROM teacher LEFT JOIN schedule ON teacher.id=schedule.id where schedule.classFrom=?", (currTime,))
+                            "SELECT DISTINCT teacher.id,teacher.name,teacher.email,\
+                            teacher.notify_id,schedule.subject,schedule.classFrom,\
+                            schedule.classTo FROM teacher LEFT JOIN schedule ON \
+                            teacher.id=schedule.id where schedule.classFrom=?", 
+                            (currTime,)
+                        )
     data=[]
     for row in sql_cursor.fetchall():
-        record={"id" : row[0],"name" : row[1],"email" : row[2],"notify_id":row[3],"subject" : row[4],"classFrom" : row[5],"classTo" : row[6]}
+        record={
+                    "id" : row[0],
+                    "name" : row[1],
+                    "email" : row[2],
+                    "notify_id":row[3],
+                    "subject" : row[4],
+                    "classFrom" : row[5],
+                    "classTo" : row[6]
+                }
         data.append(record)
         for dataCount in range (0,len(data)):
             print(data[dataCount])
             with app.app_context():
                 #mail code
-                msg = Message('Hello', sender = 'mailmeonpunit@gmail.com', recipients = [data[dataCount]["email"]])
-                msg.body = "Hello Mr. "+data[dataCount]["name"]+", Its time for the class of subject "+data[dataCount]["subject"]+" from "+data[dataCount]["classFrom"]+" to "+data[dataCount]["classTo"]+"."
+                msg = Message(
+                                'Class schedule alert', 
+                                sender = 'mailmeonpunit@gmail.com', 
+                                recipients = [data[dataCount]["email"]]
+                            )
+                msg.body = "Hello Mr. "\
+                            +data[dataCount]["name"]\
+                            +", Its time for the class of subject "\
+                            +data[dataCount]["subject"]\
+                            +" from "+data[dataCount]["classFrom"]\
+                            +" to "+data[dataCount]["classTo"]+"."
                 try:
                     mail.send(msg)
                 except Exception as e:
                     print(e)
                 print("Sent")
                 # notification code
-                header = {"Content-Type": "application/json; charset=utf-8","Authorization": "Basic OWIyYWUyZGEtODhiMS00M2EwLWFiNzItZTJkODI0YmRjYjMz"}
-
-                payload = {"app_id": "e9af5911-ad44-4157-bd6c-6cfabf2374de","include_player_ids": [data[dataCount]["notify_id"]],"contents": {"en": msg.body}}
-                    
-                req = requests.post("https://onesignal.com/api/v1/notifications", headers=header, data=json.dumps(payload))
-                print(req.status_code, req.reason)
+                send_push_notification([data[dataCount]["notify_id"]],msg.body)
+                
     # to notify 5 minutes prior to class end
     sql_cursor.execute(
-    "SELECT DISTINCT teacher.id,teacher.name,teacher.email,teacher.notify_id,schedule.subject,schedule.classFrom,schedule.classTo FROM teacher LEFT JOIN schedule ON teacher.id=schedule.id where schedule.classTo=?", (currTime,))
+                            "SELECT DISTINCT teacher.id,teacher.name,teacher.email,\
+                            teacher.notify_id,schedule.subject,schedule.classFrom,\
+                            schedule.classTo FROM teacher LEFT JOIN schedule ON \
+                            teacher.id=schedule.id where schedule.classTo=?", 
+                            (currTime,)
+                       )
     data=[]
     for row in sql_cursor.fetchall():
-        record={"id" : row[0],"name" : row[1],"email" : row[2],"notify_id":row[3],"subject" : row[4],"classFrom" : row[5],"classTo" : row[6]}
+        record={
+                    "id" : row[0],
+                    "name" : row[1],
+                    "email" : row[2],
+                    "notify_id":row[3],
+                    "subject" : row[4],
+                    "classFrom" : row[5],
+                    "classTo" : row[6]
+                }
         data.append(record)
         for dataCount in range (0,len(data)):
             print(data[dataCount])
             with app.app_context():
                 #mail code
-                msg = Message('Hello', sender = 'mailmeonpunit@gmail.com', recipients = [data[dataCount]["email"]])
-                msg.body = "Hello Mr. "+data[dataCount]["name"]+", Its time to wrap up class as this class will end on: "+data[dataCount]["classTo"]+"."
+                msg = Message(
+                                'Class schedule alert', 
+                                sender = 'mailmeonpunit@gmail.com', 
+                                recipients = [data[dataCount]["email"]]
+                             )
+                msg.body = "Hello Mr. "\
+                            +data[dataCount]["name"]\
+                            +", Its time to wrap up class as this class will end on: "\
+                            +data[dataCount]["classTo"]+"."
                 try:
                     mail.send(msg)
                 except Exception as e:
                     print(e)
                 print("Sent")
                 # notification code
-                header = {"Content-Type": "application/json; charset=utf-8","Authorization": "Basic OWIyYWUyZGEtODhiMS00M2EwLWFiNzItZTJkODI0YmRjYjMz"}
-
-                payload = {"app_id": "e9af5911-ad44-4157-bd6c-6cfabf2374de","include_player_ids": [data[dataCount]["notify_id"]],"contents": {"en": msg.body}}
-                    
-                req = requests.post("https://onesignal.com/api/v1/notifications", headers=header, data=json.dumps(payload))
-                print(req.status_code, req.reason)
+                send_push_notification([data[dataCount]["notify_id"]],msg.body)
+                
     sql_db.commit()
     sql_cursor.close()
     sql_db.close()
@@ -221,16 +169,16 @@ def task():
 
 
 if __name__ == '__main__':
-    import sqlite3
-    basedir=os.path.abspath(os.path.dirname(__file__))
     sql_db = sqlite3.connect(basedir+'/assignment.db')
     sql_cursor = sql_db.cursor()
     sql_cursor.execute(
-    '''CREATE TABLE IF NOT EXISTS teacher(
-       id INT PRIMARY KEY, name TEXT, email TEXT,notify_id TEXT)''')
+                        '''CREATE TABLE IF NOT EXISTS teacher(
+                        id INT PRIMARY KEY, name TEXT, email TEXT,notify_id TEXT)'''
+                    )
     sql_cursor.execute(
-    '''CREATE TABLE IF NOT EXISTS schedule(
-       id INT, subject TEXT, classFrom TIME,classTo TIME)''')
+                        '''CREATE TABLE IF NOT EXISTS schedule(
+                        id INT, subject TEXT, classFrom TIME,classTo TIME)'''
+                        )
     sql_db.commit()
     sql_cursor.close()
     sql_db.close()
